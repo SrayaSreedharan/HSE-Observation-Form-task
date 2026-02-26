39f619f30212655c0febc37adcc2d07185b19f50
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2026-02-26 18:37:27 +0530

    Observation and summery page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SummeryPage from "./Summerypage";
 import ObservationPage from "./Observationpage";
+import SummeryPage from "./summerypage";
+
 
 function App() {
   return (
